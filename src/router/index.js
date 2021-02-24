dbe2f048261ac91df83000c59a7a1662ccdf2621
zjh-develop <zhangjiahua>
2021-02-24 17:43:46 +0800

    。

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from 'vue';
+import Router from 'vue-router';
+import HelloWorld from '@/components/HelloWorld';
+import Login from '@/pages/login/Login';
+import Home from '@/pages/home';
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+    {path: '/',name: 'HelloWorld',component: HelloWorld},
+    {path: '/login', component: Login, hidden: true },
+    {path: '/home', component: Home, hidden: true },
   ]
 })
